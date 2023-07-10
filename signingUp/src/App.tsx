126c1fb63624b0bae2d58b083cbd93b060b207d9
fremont <fremont.babra@gmail.com>
2023-07-10 18:54:18 +0300

    first commit

---
+++ signingUp/src/App.tsx
@@ -0,0 +1,13 @@
+// import Main from "./Components/Main.js";
+import SignUp from "./Components/SignUp.jsx";
+
+function App() {
+  return (
+    <div>
+      <SignUp />
+      {/* <Main /> */}
+    </div>
+  );
+}
+
+export default App;
